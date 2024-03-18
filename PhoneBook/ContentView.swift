d6d67a18068b7e753a91042f73142d55403b522b
sirx2713 <bradx2713@gmail.com>
2024-03-18 12:22:48 +0300

    Initial Commit

---
+++ PhoneBook/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PhoneBook
+//
+//  Created by Tafadzwa Alexander Razaro on 2024/03/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PhoneBook/PhoneBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhoneBookApp.swift
+//  PhoneBook
+//
+//  Created by Tafadzwa Alexander Razaro on 2024/03/18.
+//
+
+import SwiftUI
+
+@main
+struct PhoneBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
